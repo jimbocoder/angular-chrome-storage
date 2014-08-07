b87bb6fb911b956694c4c36d9cf02361b0c88d1b
Jim Howell <jim.howell@frontgatetickets.com>
2014-08-06 21:03:19 -0500

    Rewrite to support chrome-storage and localstorage with the same async interface.  Support serializing/deserializing objects on the fly for local storage to emulate chrome storage.

--- localwrapper.js
+++ localwrapper.js
@@ -1,74 +1,125 @@
-/**
- * Created by ejf3 on 12/7/13.
- */
-'use strict';
+(function(window, angular, undefined) {
+    'use strict';
 
-angular.module('angular-chrome-localstorage', [ 'ngResource' ])
-    .service('LocalWrapper', function ($rootScope) {
-        var chromeStore = null;
-        (function(){
-            // init the chromeStore if it's available
-            if (!!chrome && !!chrome.storage) {
-                chromeStore = chrome.storage.local;
+    var Codec = function() {
+        this.JSONMarker = "___JSON:";
+        this.encode = function(val) {
+            if (angular.isObject(val) || angular.isArray(val)) {
+                val = this.JSONMarker + angular.toJson(val);
+            } else if (typeof val == "undefined") {
+                val = null;
             }
-        })();
+            return val;
+        };
 
-        this.setSync = function(sync){
-            // if we want to use the chrome.storage.sync APIs, we can set that here
-            if (sync)
-                chromeStore = chrome.storage.sync;
-            else
-                chromeStore = chrome.storage.local;
-        }
+        this.decode = function(val) {
+            if (typeof val === 'undefined') {
+                return undefined;
+            }
+            if (val.slice(0, this.JSONMarker.length) === this.JSONMarker) {
+                val = angular.fromJson(val.slice(this.JSONMarker.length));
+            }
+            return val;
+        };
 
-        this.get = function (key, EMIT_STR) {
-            // we need a way to inform listeners, so don't allow the EMIT_STR to be empty
-            if (!EMIT_STR || EMIT_STR === '')
-                return;
+        return this;
+    };
 
-            if (!!chromeStore) {
-                chromeStore.get(key, function(data){
-                     // chrome.storage returns an Object containing { key : value }
-                    // need to make sure to pull that out when returning
-                    $rootScope.$emit(EMIT_STR, data[key]);
-                });
-            } else {
-                // $rootScope.$emit is AngularJS's event emitter
-                $rootScope.$emit(EMIT_STR, localStorage[key]);
+    var LocalStrategy = function() {
+        var self = this;
+        this.get = function(key, cb) {
+            cb(this.Codec.decode(localStorage[key]));
+        };
+
+
+        this.set = function(key, val, cb) {
+            localStorage[key] = this.Codec.encode(val);
+            if (!!cb) {
+                cb();
             }
         };
 
-        this.set = function (key, val, EMIT_STR) {
+        this.remove = function(key, cb) {
+            delete localStorage[key];
+            if (!!cb) {
+                cb();
+            }
+        };
+
+        this.Codec = new Codec();
+    };
+
+    var ChromeStoreStrategy = function() {
+        var chromeStore;
+        this.setSync = function(useSync) {
+            chromeStore = !!useSync ? chrome.storage.sync : chrome.storage.local;
+        };
+
+
+        this.set = function(key, val, cb) {
+            // chrome.storage requires an object be passed in to the set function
             var newSomething = {};
             newSomething[key] = val;
-
-            if (!!chromeStore) {
-                // chrome.storage requires an object be passed in to the set function
-                chromeStore.set(newSomething, function(){
-                    if (!!EMIT_STR)
-                        $rootScope.$emit(EMIT_STR);
-                });
-            } else {
-                localStorage[key] = val;
-                if (!!EMIT_STR)
-                    $rootScope.$emit(EMIT_STR);
-            }
+            chromeStore.set(newSomething, function() {
+                if (!!cb) {
+                    cb();
+                }
+            });
         };
 
-        this.remove = function (key, EMIT_STR) {
-            var newSomething = {};
-            newSomething[key] = "";
+        this.get = function(key, cb) {
+            chromeStore.get(key, function(data) {
+                // chrome.storage returns an Object containing { key : value }
+                // need to make sure to pull that out when returning
+                cb(data[key]);
+            });
+        };
 
-            if (!!chromeStore) {
-                chromeStore.set(newSomething, function(){
-                    if (!!EMIT_STR)
-                        $rootScope.$emit(EMIT_STR);
-                });
+        this.remove = function(key, cb) {
+            chromeStore.remove(key, function() {
+                if (!!cb) {
+                    cb();
+                }
+            });
+        };
+    };
+
+    angular.module('angular-chrome-storage', [])
+        .service('LocalWrapper', function() {
+
+            var strategy = null;
+
+            this.setSync = function(useSync) {
+                if (typeof strategy !== "ChromeStoreStrategy") {
+                    throw new Error("Can't use sync without Chrome Storage");
+                } else {
+                    ChromeStoreStrategy.setSync(useSync);
+                }
+            };
+
+            this.setStrategy = function(s) {
+                strategy = s;
+            };
+
+            this.set = function(key, val, cb) {
+                return strategy.set(key, val, cb);
+            };
+
+            this.get = function(key, cb) {
+                return strategy.get(key, cb);
+            };
+
+            this.remove = function(key, cb) {
+                return strategy.remove(key, cb);
+            };
+
+            // Initialize to non-sync local storage, whichever type is available.
+            // Sync has to be explicitly enabled via setSync() if desired.
+            if (!!chrome && !!chrome.storage) {
+                ChromeStoreStrategy.setSync(false);
+                this.setStrategy(new ChromeStoreStrategy());
             } else {
-                localStorage[key] = val;
-                if (!!EMIT_STR)
-                    $rootScope.$emit(EMIT_STR);
+                this.setStrategy(new LocalStrategy());
             }
-        }
-
-    });
+        });
+})(window, window.angular);
